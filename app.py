2e4b060ce5addcd5e72c20003d073fd7239f1352
Saurabh Kumar <saurabhkmr10937@gmail.com>
2026-03-23 19:20:29 +0530

    Fix port issue for deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from rag_pipeline import rag_query
 
@@ -19,4 +20,5 @@ def ask():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
